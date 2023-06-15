e308ca437d4bcc49ac789fd920ae0e737c170379
Praneel Bora <praneelbora@gmail.com>
2023-06-15 13:03:39 +0530

    Dropdown bug fixed

--- navbar.php
+++ navbar.php
@@ -1,23 +1,22 @@
 
 <?php
-
     function createnavbar(){
         return ' 
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Navbar</a>
+        <a class="navbar-brand" style="margin-left:3vw;"href="#">Navbar</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+          <span class="navbar-toggler-icon" ></span>
         </button>
       
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          <ul class="navbar-nav">
+            <li class="nav-item active" style="margin-left:3vw;">
+              <a class="nav-link" href="#">Home <span class="sr-only"></span></a>
             </li>
-            <li class="nav-item">
+            <li class="nav-item" style="margin-left:3vw;">
               <a class="nav-link" href="#">Link</a>
             </li>
-            <li class="nav-item dropdown">
+            <li class="nav-item dropdown" style="margin-left:3vw;">
               <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 Dropdown
               </a>
@@ -28,21 +27,10 @@
                 <a class="dropdown-item" href="#">Something else here</a>
               </div>
             </li>
-            <li class="nav-item">
-              <a class="nav-link disabled" href="#">Disabled</a>
-            </li>
           </ul>
-          <span >
-            <form class="form-inline my-2 my-lg-0" >
-              <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-              <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-            </form>
-          </span>
           
         </div>
       </nav>
-
-
 
         ';
     }
