085c351f2cb04c8ec24122badd4d7259ddc956e7
Sushant Nair <136300038+sushantnair@users.noreply.github.com>
2023-06-19 18:41:44 +0530

    Update dash_admin.php

--- Admin/dash_admin.php
+++ Admin/dash_admin.php
@@ -28,13 +28,46 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>IM-KJSCE</title>
+    <link rel="stylesheet" type="text/css" href="../CSS/bootstrap.min.css">
+    <!-- ../ is used to go one level up from Admin folder. -->
+    <style>
+        html, body{
+            height: 100%;
+        }
+        /* the html and body elements are set to have a height of 100% using height: 100%;. This ensures that the body element will take up the full height of the viewport */
+        body{
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+        .dash_admin_box{
+            width: auto;
+            height: auto;
+            background-color: azure;
+            border: solid 2px black;
+            padding: 10px;
+        }
+        a{
+            font-size: 20px;
+            color: black;
+        }
+    </style>
+    <title>IM-KJSCE | Admin Dashboard</title>
 </head>
 <body>
-    <a href='add_assist.php'>ADD LAB ASSISTANTS</a>
-    <br>
-    <a href='add_lab.php'>ADD LABS</a>
-    <br>
-    <a href='../logout.php'>SIGN OUT</a>
+    <div class="dash_admin_box">
+        <p>Please select an option suitable
+        for the operation you want to undertake</p>
+        <button class="btn btn-primary btn-block"> <a href='add_assist.php'>Add Lab Assistants</a> </button>
+        <br>
+        <button class="btn btn-primary btn-block">
+            <a href='add_lab.php'>Add Labs</a>
+        </button>
+        <br>
+        <button class="btn btn-primary btn-block">
+            <a href='../logout.php'>Signout</a>
+        </button>
+        //changed from all caps for user convenience
+    </div>
 </body>
 </html>
