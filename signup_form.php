f25987d4a6d49a99f2385810e2ce9d5255556170
jagjitsbhumra <jagjit.s.bhumra@gmail.com>
2023-07-02 10:51:49 +0530

    Signup page somewhat redesigned :)

--- signup_form.php
+++ signup_form.php
@@ -38,67 +38,82 @@
     </div> -->
     <div class="container-fluid">
         <div class="row">
-            <div class="col-lg-6 col-md-6 col-12">
-                <img src="Signup.png" class="img-fluid">
+            <div class="col-lg-5 col-md-6 col-12 ms-0 ps-0 pt-0" style="height:100vh;">
+            <!-- <img src="Group 1.png" class="absolute img-fluid h-100"> -->
+            <img src="Signup.png" class="position-absolute img-fluid ms-0 ps-0 h-100">
             </div>
-            <div class="col-lg-6 col-md-6 col-12">
+            <div class="col-lg-6 col-md-6 col-12 mt-3">
                 <div class="container ">
                     <h2>Welcome to</h2>
                     <h2 style="color: red; font-weight: bold;">KJSCE lab Inventory Management</h2>
                     <hr>
                     <form action="signup.php" method="POST">
-                        <div class="input-group mb-6">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text">👤 </span>
-                            </div>
-                            <div class="form-floating col-lg-10 col-md-10 col-10">
+                        <!-- <div class="input-group mb-6"> -->
+                            <!-- <div class="input-group-prepend"> -->
+                            <div class="mx-auto">
+                                <span class="icon position-absolute h4" style="z-index: 100; width:60px; height: 60px; text-align:center; vertical-align:middle;">👤 </span>
+                            <!-- </div> -->
+                            <div class="form-floating col-12 mb-6">
                                 <input class="form-control" type="text" id="name" name="name" placeholder="Full Name" required>
-                                <label for="name">Full Name</label>        
+                                <label class="label ms-2" for="name">Full Name</label>        
                             </div>
-                        </div>
+                            </div>
+                        <!-- </div> -->
                         <br>
-                        <div class="input-group mb-3">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text">&#128231; </span>
-                            </div>
-                            <div class="form-floating col-lg-10 col-md-10 col-10">
+                        <!-- <div class="input-group mb-3">
+                            <div class="input-group-prepend"> -->
+                            <div class="mx-auto">
+                            <span class="icon position-absolute h4" style="z-index: 100; width:60px; height: 60px; text-align:center; vertical-align:middle;">&#128231; </span>
+                                <!-- <span class="input-group-text py-3">&#128231; </span> -->
+                            <!-- </div> -->
+                            <div class="form-floating col-12 mb-6">
                                 <input class="form-control" type="email" id="email" name="email" placeholder="Email" required pattern=".+@somaiya\.edu$" title="Please enter a valid @somaiya.edu email address">             
-                                <label for="email">Email</label>
+                                <label class="label ms-2" for="email">Email</label>
                             </div>
                         </div>    
-                        <br>      
+                        <br>
+                        <div class="row">  
+                        <!-- <div class="col-6">    
                         <div class="input-group mb-3">
                             <div class="input-group-prepend">
-                                <span class="input-group-text">🔑</span>
-                            </div>
-                            <div class="form-floating col-lg-3 col-md-3 col-6">
-                                <input class="form-control " type="password" id="pass" name="pass" placeholder="Password" required>
-                                <label for="pass">Password</label>
+                                <span class="input-group-text py-3">🔑</span>
+                            </div> -->
+                            <div class="mx-auto col-6">
+                            <span class="icon position-absolute h4" style="z-index: 100; width:60px; height: 60px; text-align:center; vertical-align:middle;">🔑 </span>
+                            
+                            <div class="form-floating col-12 mb-6">
+                                <input class="form-control" type="password" id="pass" name="pass" placeholder="Password" required>
+                                <label class="label ms-2" for="pass">Password</label>
                             </div>
                         </div>
+                        <!-- </div> -->
+                        <!-- <div class="col-6">
                         <div class="input-group mb-3">
                             <div class="input-group-prepend">
-                                <span class="input-group-text">🔒</span>
-                            </div>
-                            <div class="form-floating col-lg-3 col-md-3 col-6">
+                                <span class="input-group-text py-3">🔒</span>
+                            </div> -->
+                            <div class="mx-auto col-6">
+                            <span class="icon position-absolute h4" style="z-index: 100; width:60px; height: 60px; text-align:center; vertical-align:middle;">🔒 </span>
+                            
+                            <div class="form-floating col-12 mb-6">
                                 <input class="form-control" type="password" id="pass" name="cpass" placeholder="Confirm Password" required>
-                                <label for="pass">Confirm Password</label>
+                                <label class="label ms-2" for="pass">Confirm Password</label>
                             </div>
-            
+                        </div>
                         </div>
                         <br>
-
-                        <div class="form-floating">
-                            <select class="form-select mb-2" id="role" name="role" required>
+                        <div class="row mb-6">
+                        <div class="col-6 form-floating">
+                            <select class="form-select" id="role" name="role" required>
                                 <option value="" disabled selected>Choose a role</option>
                                 <option value="student">Student</option>
                                 <option value="lab-assistant">Lab Assistant</option>
                                 <option value="faculty">Faculty</option>
                             </select>
-                            <label for="role" class="select-label">Role</label>
+                            <label for="role" class="select-label ms-3">Role</label>
                         </div>
 
-                        <div class="form-floating">
+                        <div class="col-6 form-floating">
                         <select class="form-select" id="dept" name="dept" required>
                             <option value="" disabled selected>Choose a Department</option>
                             <?php 
@@ -113,19 +128,29 @@
                             ?>
 
                         </select>
-                        <label for="dept" class="select-label">Department</label>
+                        <label for="dept" class="select-label ms-3">Department</label>
+                        </div>
                         </div>
                         <br>
 
-                        <div class="form-floating">
+                        <!-- <div class="input-group mb-3">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text py-3 px-3">@</span>
+                            </div> -->
+                            <div class="mx-auto">
+                            <span class="icon position-absolute h4" style="z-index: 100; width:60px; height: 60px; text-align:center; vertical-align:middle;">🔑 </span>
+                            
+                            <div class="form-floating col-12 mb-6">
                             <input class="form-control" type="text" id="id" name="id" placeholder="ID Number" required>
-                            <label for="id">ID Number</label>
+                            <label class="label ms-2" for="id">ID Number</label>
                         </div>
+                        </div>  
                         <br>
 
-                        <button class="col-lg-12 col-md-10 col-10" type="submit" style="background-color: red ; color: white;">Register</button>
+                        <button class="btn btn-danger col-lg-12 col-md-10 col-10 mb-4" type="submit" style="background-color: #D40000 ; color: white; height:45px;">Register</button>
+                            </div>
                     </form>
-                <p>Already have an account? <a href="login_form.php">Login</a></p>
+                    <p style="text-align:center;">Already have an account? <a href="login_form.php">Login</a></p>
 
                 </div>
             </div>
