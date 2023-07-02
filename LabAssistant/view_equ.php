510d4437250623af8fbd6c09ff5753d3794f301f
bingereader12 <devesh.jain@somaiya.edu>
2023-07-02 12:05:46 +0530

    Added Update popup

--- LabAssistant/view_equ.php
+++ LabAssistant/view_equ.php
@@ -131,6 +131,38 @@
     {
         header('Location:../logout.php');
     }
+
+    if (isset($_POST['update'])) {
+      $eqname = $_POST['name'];
+      $eqtype = $_POST['type'];
+      $dsrno = $_POST['dsr'];
+      $quantity = $_POST['quant'];
+      $desc1 = $_POST['description1'];
+      $desc2 = $_POST['description2'];
+      $cost = $_POST['cost'];
+
+      $sql1=mysqli_query($conn,"SELECT * FROM labs WHERE assistid=$id");
+              
+      $row1 = mysqli_fetch_array($sql1,MYSQLI_ASSOC);
+      if(!$row1)
+      {
+          echo mysqli_error($conn);
+          die();
+      }
+      
+      $labno=$row1['labno'];   //LAB-NUMBER
+
+      $updateQuery = "UPDATE $labno SET eqname='$eqname', eqtype='$eqtype', dsrno='$dsrno', quantity='$quantity', desc1='$desc1', desc2='$desc2', cost='$cost' WHERE dsrno='$dsrno'";
+      $result = mysqli_query($conn, $updateQuery);
+
+      if ($result) {
+          // Update successful
+          echo "Update successful!";
+      } else {
+          // Update failed
+          echo "Update failed: " . mysqli_error($conn);
+      }
+  }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -142,7 +174,61 @@
     <!--<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">-->
     <link rel="stylesheet" href="../CSS/bootstrap.min.css">
     <!-- using an offline copy saves time spent for loading bootstrap from online source  -->
+    <style>
+    .popup {
+      display: none;
+      position: fixed;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      width: 400px;
+      background-color: #f1f1f1;
+      padding: 20px;
+      border: 1px solid #ccc;
+      border-radius: 5px;
+    }
 
+    .form-group {
+      margin-bottom: 15px;
+    }
+
+    .form-group label {
+      display: block;
+      font-weight: bold;
+    }
+
+    .form-group input,
+    .form-group textarea {
+      width: 100%;
+      padding: 5px;
+      border: 1px solid #ccc;
+      border-radius: 3px;
+    }
+
+    .form-group textarea {
+      height: 80px;
+    }
+
+    .form-group input[type="submit"] {
+      background-color: #4CAF50;
+      color: white;
+      cursor: pointer;
+    }
+
+    .form-group input[type="submit"]:hover {
+      background-color: #45a049;
+    }
+
+    .form-group button {
+      background-color: #ccc;
+      color: black;
+      cursor: pointer;
+    }
+
+    .form-group button:hover {
+      background-color: #999;
+    }
+  </style>
 </head>
 <body>
     <!-- TEMPORARY DASHBOARD -->
@@ -177,6 +263,7 @@
                     <th scope="col">Type<br></th>
                     <th scope="col">DSR</th>
                     <th scope="col">Quantity</th>
+                    <th scope="col">Lent Quantity</th>
                     <th scope="col">Description 1</th>
                     <th scope="col">Description 2</th>
                     <th scope="col">Cost</th>
@@ -219,7 +306,7 @@
 
                     //FETCH LAB TABLE USING LAB-NUMBER
                     $table=mysqli_query($conn,"SELECT * FROM $labno");
-
+                    $v=1;
                     while($row = mysqli_fetch_array($table,MYSQLI_ASSOC))
                     {
                         ?>
@@ -227,7 +314,8 @@
                             <td><?php echo $row['eqname'];?></td>
                             <td><?php echo $row['eqtype'];?></td>
                             <td><?php echo $row['dsrno'];?></td>
-                            <td><?php echo $row['quantity']." + ".$row['toquan'];?> </td>
+                            <td><?php echo $row['quantity'];?></td>
+                            <td><?php echo $row['toquan']?></td>
                             <td><?php echo $row['desc1'];?></td>
                             <td><?php echo $row['desc2'];?></td>
                             <td><?php echo $row['cost'];?></td>
@@ -236,11 +324,11 @@
                             if($row['byquan']==0)
                             {
                                 ?>
-                                <form action="view_equ.php" method="post">
-                                    <input type="text" name="dsrno" value="<?php echo $row['dsrno']; ?>" style="display:none;">
-                                    <button class="button1" type="submit" name="update"> 
+                                <button class="button1" onclick="openPopup(<?php echo$v;?>)"> 
                                         Update
                                     </button>
+                                <form action="view_equ.php" method="post">
+                                <input type="text" name="dsrno" value="<?php echo $row['dsrno']; ?>" style="display:none;">
                                     <button class="button1" type="submit" name="delete"> 
                                         Delete
                                     </button>
@@ -270,11 +358,74 @@
                             </td>
                         </tr>
                         <?php
+                        $v=$v+1;
                     }
                 ?>
             </tbody>
         </table>
     </div>
+    <div id="popup" class="popup">
+    <h2>Update Form</h2>
+    <form action="" method="post" id="updateForm">
+      <div class="form-group">
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="name">
+      </div>
+      <div class="form-group">
+        <label for="type">Type:</label>
+        <input type="text" id="type" name="type">
+      </div>
+      <div class="form-group">
+        <label for="dsr">DSR No.:</label>
+        <input type="text" id="dsr" name="dsr">
+      </div>
+      <div class="form-group">
+        <label for="quantity">Quantity:</label>
+        <input type="number" id="quant" name="quant">
+      </div>
+      <div class="form-group">
+        <label for="description1">Description 1:</label>
+        <textarea id="description1" name="description1"></textarea>
+      </div>
+      <div class="form-group">
+        <label for="description2">Description 2:</label>
+        <textarea id="description2" name="description2"></textarea>
+      </div>
+      <div class="form-group">
+        <label for="cost">Cost:</label>
+        <input type="number" id="costin" name="cost">
+      </div>
+      <div class="form-group">
+        <input type="submit" name="update" value="update">
+      </div>
+    </form>
+    <button onclick="closePopup()">Close</button>
+  </div>
 
+  <script>
+function openPopup(rowId) {
+  var popup = document.getElementById("popup");
+  popup.style.display = "block";
+
+  // Get the data from the table row
+  var tableRows = document.getElementsByTagName("tr");
+  var targetRow = tableRows[rowId + 1]; // Add 1 to skip the form row
+  var cells = targetRow.getElementsByTagName("td");
+
+  // Populate the form fields with the data
+  document.getElementById("name").value = cells[0].innerHTML;
+  document.getElementById("type").value = cells[1].innerHTML;
+  document.getElementById("dsr").value = cells[2].innerHTML;
+  document.getElementById("quant").value = cells[3].innerText;
+  document.getElementById("description1").value = cells[5].innerHTML;
+  document.getElementById("description2").value = cells[6].innerHTML;
+  document.getElementById("costin").value = cells[7].innerHTML;
+}
+
+function closePopup() {
+      var popup = document.getElementById("popup");
+      popup.style.display = "none";
+    }
+  </script>
 </body>
 </html>
