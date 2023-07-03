34760ce42ca491ab33ebd0fbe2600c5258f1e183
Shashank B <mshashank033@gmail.com>
2023-07-03 23:04:40 +0530

    Added Sidebar in student-dash

--- Student/dash.php
+++ Student/dash.php
@@ -23,34 +23,80 @@
         header('Location:../logout.php');
     }
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>IM-KJSCE</title>
-    <!--<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="../CSS/bootstrap.min.css">
-    <!-- using an offline copy saves time spent for loading bootstrap from online source  -->
     <link rel="stylesheet" href="CSS/styles.css">
+    <style>
+    
+        /* Desktop view */
+        @media (min-width: 768px) {
+            .sidebar {
+                width: 20%;
+                float: left;
+                background-color: azure; /* Add the background color */
+
+            }
+            .content {
+                width: 80%;
+                float: right;
+            }
+        }
+
+        /* Mobile view */
+        @media (max-width: 767px) {
+            .sidebar {
+                display: none;
+                background-color: azure; /* Add the background color */
+
+            }
+            .content {
+                width: 100%;
+            }
+            
+        }
+        /* Logo styles */
+        .logo {
+            width: 250px; /* Adjust the size as needed */
+            margin: 20px; /* Add margin to position the logo */
+            position: absolute;
+            top: 10px; /* Adjust the top value to position the logo vertically */
+            left: 10px; /* Adjust the left value to position the logo horizontally */
+        
+        }
+
+        
+    </style>
 </head>
 <body>
-    
-<div class="dash_lab_box">
-        
-        <h6><span style="float: left; text-decoration: underline;">User ID: <?php echo $id; ?></span><span style="float: right;">Role: Student</span></h6><br>        
+
+    <div class="sidebar">
+    <img src="SomaiyaLogo.jpg" alt="Logo" class="logo">
+
+        <h6><u>User ID:</u> <?php echo $id; ?></h6>
+        <h6><u>Role:</u> Student</h6>
+        <br>
         <p>Please select an option suitable for the operation you want to undertake</p>
-        <button class="btn btn-primary btn-block" onclick="window.location.href='view.php'"> 
-            View Labs
-        </button>
-        <br>
-        <button class="btn btn-primary btn-block" onclick="window.location.href='equ.php'"> 
-            View Equipment and Requests
-        </button>
-        <br>
-        <button class="btn btn-primary btn-block" onclick="window.location.href='../logout.php'"> 
-           Signout
-        </button>
+        <button class="btn btn-primary btn-block" onclick="window.location.href='view.php'">View Labs</button>
+        <br><br>
+        <button class="btn btn-primary btn-block" onclick="window.location.href='equ.php'">View Equipment and Requests</button>
+        <br><br><br><br><br><br>
+        <button class="btn btn-primary btn-block" onclick="window.location.href='../logout.php'">Signout</button>
     </div>
-    </body>
+
+    <div class="content">
+        <!-- Your content here -->
+    </div>
+
+    <!-- Scripts -->
+    <!-- Include Bootstrap and other scripts here -->
+    <script src="../JS/bootstrap.min.js"></script>
+
+</body>
 </html>
